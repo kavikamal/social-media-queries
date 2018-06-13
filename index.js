095975a6b126c066b72ced4e5aeef19ac01cec06
Kavitha Kamal <kavithakamal@gmail.com>
2018-06-13 12:14:09 -0400

    Completed till step 8 for the assesment

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.get('/users/:id', (req, res) => {
 });
 
 app.post('/users',(req, res) =>{
-    const text = 'INSERT INTO users (username, bio) VALUES ($1, $2)';
+    const text = 'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *';
     const values = ['kenzie', 'Kenzie Academy is a user experience design and coding school in Indianapolis, Indiana. Our 6-month to 2-year program with 1-year paid apprenticeship is a new alternative to traditional colleges and short-term coding bootcamps. Students have the option of attending the program in person, or remotely via our Hybrid Program.'];
     client.query(text, values, (err, result) => {
         console.log(result.rows[0]);
     });
-});
+}); 
 
 // start a server that listens on port 3000 and connects the sql client on success
 app.listen(3000, () => {
