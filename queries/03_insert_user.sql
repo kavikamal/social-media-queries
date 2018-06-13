2d4757750cbe093e54f9d0373bf3b44818626b99
Kavitha Kamal <kavithakamal@gmail.com>
2018-06-13 13:54:10 -0400

    Completed till step 8 for the assesment

--- queries/03_insert_user.sql
+++ queries/03_insert_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (user_name,bio) VALUES ('admin','I am just a lonely administrator.');
-INSERT INTO users (user_name,bio) VALUES ('user1','I am a awesome user.');
+INSERT INTO users (username,bio) VALUES ('admin','I am just a lonely administrator.');
+INSERT INTO users (username,bio) VALUES ('user1','I am a awesome user.');
