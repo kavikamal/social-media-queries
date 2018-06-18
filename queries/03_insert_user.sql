d7513303d59815686a4da7f0a2a9a55beae174b0
Kavitha Kamal <kavithakamal@gmail.com>
2018-06-18 00:40:06 -0400

    Modified insert user sql

--- queries/03_insert_user.sql
+++ queries/03_insert_user.sql
@@ -1,2 +1,1 @@
-INSERT INTO users (username,bio) VALUES ('admin','I am just a lonely administrator.');
-INSERT INTO users (username,bio) VALUES ('user1','I am a awesome user.');
+INSERT INTO users (username,bio) VALUES ('admin','I am just a lonely administrator.') , ('user1','I am a awesome user.');
